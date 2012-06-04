866368685c1d30fea10b017dd049f6135aff1547
Manuel Simoni <msimoni@gmail.com>
2012-06-04 18:25:45 +0200

    Document sync algorithm.

--- lolstorage.js
+++ lolstorage.js
@@ -11,6 +11,11 @@ var lol = (function() {
         return PREFIX + "_" + storeID + "_" + key;
     }
 
+    /*
+      LOCAL STORE
+      ------------------------------------------------------------------
+    */
+
     /** Creates a new localStorage content store with the given ID.
         IDs allow the use of independent "storage areas" in the
         browser's single localStorage namespace.  The ID should not
@@ -51,7 +56,11 @@ var lol = (function() {
         setTimeout(doit, 1);
     };
 
-    /* RemoteStorage store. */
+    /*
+      REMOTE STORE
+      ------------------------------------------------------------------
+    */
+
     function RemoteStore(id, client) {
         this.id = id;
         this.client = client;
@@ -62,26 +71,33 @@ var lol = (function() {
     };
 
     RemoteStore.prototype.get = function(key, cb) {
-        this.client.get(storageKey(key, this.id), function(error, data) {
+        var store = this;
+        this.client.get(storageKey(key, store.id), function(error, data) {
             if (error) {
                 cb(false, error);
             } else {
+                console.log("Get " + key +  " from " + store);
                 cb(true, data);
             }
         });
     };
     
     RemoteStore.prototype.put = function(key, value, cb) {
-        this.client.put(storageKey(key, this.id), value, function(error) {
+        var store = this;
+        this.client.put(storageKey(key, store.id), value, function(error) {
             if (error) {
                 cb(false, error);
             } else {
+                console.log("Put " + key +  " to " + store);
                 cb(true);
             }
         });
     };
 
-    // Version control objects
+    /*
+      OBJECTS
+      ------------------------------------------------------------------
+    */
 
     var BLOB_TYPE = "blob";
     var TREE_TYPE = "tree";
@@ -128,69 +144,96 @@ var lol = (function() {
         }
     }
     
-    /** Pulls an object (typically a tree) identified by srcHash from
-        the source store (src) to the destination store (dst) whose
-        current state is dstHash (or null if destination is
-        empty/uninitialized). */
+    /*
+      SYNCHRONIZATION
+      ------------------------------------------------------------------
+    */
+
+    /** Transfers an object (typically a tree) identified by srcHash
+        from the source store (src) to the destination store (dst)
+        whose current state is dstHash (or null if destination is
+        empty/uninitialized).
+
+	The main invariant exploited by this code is that if a tree
+        exists in a store, all its entries are assumed to exist in the
+        store, too. */
     function sync(src, srcHash, dst, dstHash, cb) {
-        dst.get(srcHash,
-                function(ok, res) {
-                    if (ok) {
-                        if (res !== undefined) {
-                            // Source object already in destination? We're done.
-                            cb(true);
+        dst.get(srcHash, function(ok, res) {
+            if (ok) {
+                if (res !== undefined) {
+                    // Source object already in destination store.
+                    // We're done.
+                    cb(true);
+                } else {
+		    // Source object not in destination store.  Fetch
+                    // source object from source store and perform its
+                    // type-specific sync logic.
+                    src.get(srcHash, function(ok, res) {
+                        if (ok) {
+                            if (res !== undefined) {
+                                var srcObj = parse(res);
+                                srcObj.sync(src, srcHash, dst, dstHash, cb);
+                            } else {
+                                cb(false, "Source object " + srcHash + 
+                                   " missing in source store " + src);
+                            }
                         } else {
-                            // Fetch source object and perform its custom sync logic.
-                            src.get(srcHash, function(ok, res) {
-                                if (ok) {
-                                    if (res !== undefined) {
-                                        var srcObj = parse(res);
-                                        srcObj.sync(src, srcHash, dst, dstHash, cb);
-                                    } else {
-                                        cb(false, "Source object " + srcHash + 
-                                           " missing in source store " + src);
-                                    }
-                                } else {
-                                    cb(false, res);
-                                }
-                            });
+                            cb(false, res);
                         }
-                    } else {
-                        cb(false, res);
-                    }
-                });
+                    });
+                }
+            } else {
+                cb(false, res);
+            }
+        });
     }
 
     Blob.prototype.sync = function(src, srcHash, dst, dstHash, cb) {
+	// Simply update the blob.
         dst.put(hash(this), content(this), cb);
     };
 
     Tree.prototype.sync = function(src, srcHash, dst, dstHash, cb) {
         var srcObj = this;
         if (dstHash !== null) {
+	    // Destination store has a current state. Fetch it.
             dst.get(dstHash, function(ok, res) {
                 if (ok) {
                     if (res !== undefined) {
                         var dstObj = parse(res);
                         if (dstObj.lol_type === TREE_TYPE) {
+			    // Destination's current state is a tree, too.
+			    // Diff against it.
                             treeSync(src, srcObj, dst, dstObj, cb);
                         } else {
+			    // Destination's current state is not a
+			    // tree.  Diff against null.
                             treeSync(src, srcObj, dst, null, cb);
                         }
                     } else {
-                        treeSync(src, srcObj, dst, null, cb);                        
+                        cb(false, "Destination object " + dstHash + 
+                           " missing in destination store " + dst);
                     }
                 } else {
                     cb(false, res);
                 }
             });
         } else {
+	    // Destination store has no current state.  Diff against null.
             treeSync(src, srcObj, dst, null, cb);
         }
     };
 
+    /** Sync a source tree against a destination tree (or null for the
+	empty tree).  Exploit inertia: if two entries (especially
+	sub-trees) that have changed have the same file name in the
+	source and destination trees, they're probably similar. */
     function treeSync(src, srcTree, dst, dstTree, cb) {
-        // This callback gets called after all children have been synced.
+
+        // This callback gets called after all child entries have been
+        // synced, and stores the tree in the destination store.  This
+        // ensures the invariant that a tree only gets stored after
+        // all its entries have been stored.
         function treeCB(ok) {
             if (ok) {
                 dst.put(hash(srcTree), content(srcTree), cb);
@@ -199,27 +242,41 @@ var lol = (function() {
             }
         }
 
+	// Compute a list of differences between source and
+	// destination tree.
         var srcEntries = srcTree.lol_entries;
         var dstEntries = (dstTree !== null) ? dstTree.lol_entries : {};
-        var diff = [];
-
+        var diffs = [];
         for (var name in srcEntries) {
             var srcHash = srcEntries[name];
             var dstHash = dstEntries[name];
             if (dstHash === undefined) {
-                diff.push({ srcHash: srcHash, dstHash: null });
+		// Entry in source tree but not in destination tree.
+		// Diff entry in source tree against null.
+                diffs.push({ srcHash: srcHash, dstHash: null });
             } else if (dstHash !== srcHash) {
-                diff.push({ srcHash: srcHash, dstHash: dstHash });
+		// Entry in source tree and destination tree but with
+		// different hashes.  Diff entry in source tree
+		// against entry in destination tree.
+                diffs.push({ srcHash: srcHash, dstHash: dstHash });
             }
+	    // Else: Unchanged entry.  Nothing to do.
         }
 
-        if (diff.length > 0) {
-            var multiCB = multiCallback(diff.length, treeCB);
-            for (var i = 0; i < diff.length; i++) {
-                var entry = diff[i];
+	// Construct a multi-callback for combining the many callbacks
+	// for each difference from the previous step, and sync each
+	// of them.
+        if (diffs.length > 0) {
+            var multiCB = multiCallback(diffs.length, treeCB);
+            for (var i = 0; i < diffs.length; i++) {
+                var diff = diffs[i];
                 sync(src, entry.srcHash, dst, entry.dstHash, multiCB);
             }
         } else {
+	    // No changed entries.  Store tree.
+	    // FIXME: A weird case.  Could happen if two trees with
+	    // the same entries are hashed differently, e.g. because
+	    // of JSON formatting differences.
             treeCB(true);
         }
     }
--- test/pullTest.js
+++ test/pullTest.js
@@ -11,12 +11,15 @@ function consoleCB(ok, res) {
 var s1 = new lol.LocalStore("s1");
 var s2 = new lol.LocalStore("s2");
 
-var file = new lol.Blob("Hello world!");
+var file1 = new lol.Blob("Hello world!");
+var file2 = new lol.Blob("Cruel2");
 
-var subTree = new lol.Tree({ "hello-world.txt": lol.hash(file) });
+var subTree = new lol.Tree({ "hello-world.txt": lol.hash(file1),
+                             "cruelty.txt": lol.hash(file2) });
 var tree = new lol.Tree({ "sub-tree": lol.hash(subTree) });
 
-s1.put(lol.hash(file), lol.content(file), consoleCB);
+s1.put(lol.hash(file1), lol.content(file1), consoleCB);
+s1.put(lol.hash(file2), lol.content(file2), consoleCB);
 s1.put(lol.hash(subTree), lol.content(subTree), consoleCB);
 s1.put(lol.hash(tree), lol.content(tree), consoleCB);
 
